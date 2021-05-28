dedfd9950aa48a9e923f249077c6ba5f0a2bbaf9
WeeSing <posttomichael@gmail.com>
2021-05-28 20:37:32 +0800

    Fixed argument reading in main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 import boot from './src/boot/boot';
-const testMode = process.argv[0] === 'test_mode';
+import logger from './src/lib/logger';
+const testMode = process.argv[2] === 'testmode';
+logger.info(`Test mode: ${testMode}`);
 boot(testMode);
