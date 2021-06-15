c944a9d5a10e89cf6dbe9543b6580492d758bdb0
Yap Wee Sing <posttomichael@gmail.com>
2021-06-15 15:58:35 +0800

    Send config to test peripheral.

--- src/logic/ble_engine_test.js
+++ src/logic/ble_engine_test.js
@@ -17,6 +17,47 @@ export class BLEEngineTest extends BLEEngine {
     this.peripheralIds = [this.testMAC];
   }
 
+  async onPeripheralSubscribed(peripheralId) {
+    super.onPeripheralSubscribed(peripheralId);
+    const characteristic =
+      this.connectionManager.getPeripheralCharacteristics(peripheralId);
+    if (_.isNil(characteristic)) {
+      return;
+    }
+
+    switch (peripheralId) {
+      case this.testMAC: {
+        // Send all the settings.
+        const mainServoSettings = _.get(config, `lock.settings.main_servo`);
+        const linearServoSettings = _.get(config, `lock.settings.linear_servo`);
+        const adxlSettings = _.get(config, `lock.settings.adxl`);
+
+        const delimiter = `\r\n`;
+        let sendData = `m_unlk=${mainServoSettings.frequencies.unlock}${delimiter}`;
+        characteristic.write(Buffer.from(sendData));
+        sendData = `m_lk=${mainServoSettings.frequencies.lock}${delimiter}`;
+        characteristic.write(Buffer.from(sendData));
+        sendData = `m_idle=${mainServoSettings.frequencies.idle}${delimiter}`;
+        characteristic.write(Buffer.from(sendData));
+
+        sendData = `l_en=${linearServoSettings.angles.engaged}${delimiter}`;
+        characteristic.write(Buffer.from(sendData));
+        sendData = `l_disen=${linearServoSettings.angles.disengaged}${delimiter}`;
+        characteristic.write(Buffer.from(sendData));
+        sendData = `l_step=${linearServoSettings.step}${delimiter}`;
+        characteristic.write(Buffer.from(sendData));
+
+        sendData = `adxl_rdcnt=${adxlSettings.max_read_count}${delimiter}`;
+        characteristic.write(Buffer.from(sendData));
+        sendData = `adxl_lk=${adxlSettings.angles.locked}${delimiter}`;
+        characteristic.write(Buffer.from(sendData));
+        sendData = `adxl_unlk=${adxlSettings.angles.unlocked}${delimiter}`;
+        characteristic.write(Buffer.from(sendData));
+        break;
+      }
+    }
+  }
+
   async onDataReceived(peripheral, data, isNotification) {
     super.onDataReceived(peripheral, data, isNotification);
 
