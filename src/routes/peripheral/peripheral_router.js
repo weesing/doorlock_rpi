52a144b562a0ced091b82405232369f33c890403
Yap Wee Sing <posttomichael@gmail.com>
2021-06-16 07:03:59 +0800

    Changed peripheral init route.

--- src/routes/peripheral/peripheral_router.js
+++ src/routes/peripheral/peripheral_router.js
@@ -2,8 +2,8 @@ import express from 'express';
 
 export let router = express.Router();
 
-router.post('/:peripiheralId/init', function (req, res, next) {
-  console.log(req);
+router.post('/init', function (req, res, next) {
+  console.log(require('util').inspect(req, { depth: 10 }));
   res.jsonp({
     message: 'Initialization triggered'
   });
