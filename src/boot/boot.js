434b0458d0566dbe857842fd9d20e06a15da4b50
Yap Wee Sing <posttomichael@gmail.com>
2021-06-16 07:13:01 +0800

    Implement peripheral init route. Global variable for engine instance.

--- src/boot/boot.js
+++ src/boot/boot.js
@@ -3,16 +3,19 @@ import { BLEEngine } from '../logic/ble_engine';
 import config from '../lib/config';
 import logger from '../lib/logger';
 
+let g_EngineInstance;
+
 var boot = async function (testMode = false) {
   logger.info(`Configuration loaded`);
   logger.info(config);
 
   if (testMode) {
     logger.warn(`Running in test mode.`);
-    await BLEEngineTest.getInstance().initBLE();
+    g_EngineInstance = BLEEngineTest.getInstance();
   } else {
-    await BLEEngine.getInstance().initBLE();
+    g_EngineInstance = BLEEngine.getInstance();
   }
+  g_EngineInstance.initBLE();
 };
 
 export default boot;
--- src/routes/peripheral/peripheral_router.js
+++ src/routes/peripheral/peripheral_router.js
@@ -4,6 +4,11 @@ export let router = express.Router();
 
 router.post('/init', function (req, res, next) {
   console.log(require('util').inspect(req, { depth: 10 }));
+  const body = req.body;
+  const peripheralId = body.peripheralId;
+  if (peripheralId) {
+    g_EngineInstance.sendInitialPeripheralSync(peripheralId);
+  }
   res.jsonp({
     message: 'Initialization triggered'
   });
