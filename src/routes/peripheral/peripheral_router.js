ad799a13fedb68191c0d58a9e76f7b0c81888fd8
Yap Wee Sing <posttomichael@gmail.com>
2021-06-16 09:01:56 +0800

    Fixed refactored function name.

--- src/routes/peripheral/peripheral_router.js
+++ src/routes/peripheral/peripheral_router.js
@@ -7,9 +7,7 @@ router.post('/init', function (req, res, next) {
   const body = req.body;
   const peripheralId = body.peripheralId.toLowerCase();
   if (peripheralId) {
-    StaticGlobals.getInstance()
-      .getVar('ble_engine')
-      .sendInitialPeripheralSync(peripheralId);
+    StaticGlobals.getInstance().getVar('ble_engine').sendSetting(peripheralId);
   }
   res.jsonp({
     message: 'Initialization triggered'
