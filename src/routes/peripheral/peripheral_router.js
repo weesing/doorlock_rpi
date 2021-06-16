1ec2c6d951c4e17a55b78e5dc6e5394821f26806
Yap Wee Sing <posttomichael@gmail.com>
2021-06-16 09:05:36 +0800

    Fix wrong function name again.

--- src/routes/peripheral/peripheral_router.js
+++ src/routes/peripheral/peripheral_router.js
@@ -13,7 +13,9 @@ router.post('/settings', (req, res, next) => {
   const body = req.body;
   const peripheralId = body.peripheralId.toLowerCase();
   if (peripheralId) {
-    StaticGlobals.getInstance().getVar('ble_engine').sendSetting(peripheralId);
+    StaticGlobals.getInstance()
+      .getVar('ble_engine')
+      .sendPeripheralSettings(peripheralId);
   }
   res.jsonp({
     message: 'Initialization triggered'
