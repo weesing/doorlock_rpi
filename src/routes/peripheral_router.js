eeb901c381d7224318a6f0dd8ed579dbb19d9bd6
Yap Wee Sing <posttomichael@gmail.com>
2021-06-18 07:50:30 +0800

    Fix import path.

--- src/routes/peripheral_router.js
+++ src/routes/peripheral_router.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { StaticGlobals } from '../../lib/static_globals';
+import { StaticGlobals } from '../lib/static_globals';
 
 export let router = express.Router();
 
