6dfac2a20271bc8dd6b90d4dbc50df5af5e789a6
WeeSing <posttomichael@gmail.com>
2021-06-18 08:24:36 +0800

    Read auth from header

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,7 +1,11 @@
 import { SecretsLoader } from "./secrets_loader";
 
 export function isValidRequest(req) {
-  const reqAuthSecret = req.body['auth'] || req.query['auth'];
+  const authorization = req.headers[`authorization`];
+  if (!authorization) {
+    return false;
+  }
+  const reqAuthSecret = authorization.split(' ')[1];
   if (!reqAuthSecret) {
     return false;
   }
