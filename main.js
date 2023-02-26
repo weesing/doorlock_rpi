d491b7256b53e1000bbd531f040ecd942865276e
Yap Wee Sing <posttomichael@gmail.com>
2023-02-26 21:03:06 +0800

    Changed app port

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ function onListening() {
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '8080');
+var port = normalizePort(process.env.PORT || '8089');
 app.set('port', port);
 
 /**
